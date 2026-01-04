1a580a8f7e48e7816e15d948d1aa111ae425bba7
Pablo <taddeiApablo23@gmail.com>
2026-01-03 23:43:54 -0300

    fix: aplicando nuevamente cambios para probar en railway

--- frontend_security_scanner/services/pdf.service.js
+++ frontend_security_scanner/services/pdf.service.js
@@ -18,7 +18,7 @@ async function generatePDF(reportHtml) {
   const page = await browser.newPage();
 
   await page.setContent(reportHtml, {
-    waitUntil: 'load'
+    waitUntil: 'networkidle'
   });
 
   const pdfBuffer = await page.pdf({
